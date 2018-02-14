43a3c6579a568c68f8d8077a1301e1c20101a0a1
David Alberto Medina Medina <david@github.com>
2018-02-14 12:28:13 +0000

    Databaser v1.2

--- src/aco_practica1/ACO_Practica1.java
+++ src/aco_practica1/ACO_Practica1.java
@@ -7,7 +7,7 @@ public class ACO_Practica1 {
 
     public static void main(String[] args) {
         
-        System.out.println("===== CASO 1 ======");
+        System.out.println("===== CASO 1: GENERAL ======");
         Path path = Paths.get("grafo1.txt");
         Grafo g1 = new Grafo(path);
         Kruskal kruskal = new Kruskal(g1);
@@ -16,7 +16,7 @@ public class ACO_Practica1 {
         kruskal.mostrarConjuntos(result.getnNodos());
         result.mostrarGrafo();
         
-        System.out.println("===== CASO 2 ======");
+        System.out.println("===== CASO 2:  ======");
         path = Paths.get("grafo2.txt");
         Grafo g2 = new Grafo(path);
         Kruskal kruskal2 = new Kruskal(g2);
@@ -32,6 +32,24 @@ public class ACO_Practica1 {
         result = kruskal3.execMST();
         System.out.println("===== RESULTADO ======");
         kruskal3.mostrarConjuntos(result.getnNodos());
+        result.mostrarGrafo();
+        
+        System.out.println("===== CASO 4 ======");
+        path = Paths.get("grafo4.txt");
+        Grafo g4 = new Grafo(path);
+        Kruskal kruskal4 = new Kruskal(g4);
+        result = kruskal4.execMST();
+        System.out.println("===== RESULTADO ======");
+        kruskal4.mostrarConjuntos(result.getnNodos());
+        result.mostrarGrafo();
+        
+        System.out.println("===== CASO 5 ======");
+        path = Paths.get("grafo5.txt");
+        Grafo g5 = new Grafo(path);
+        Kruskal kruskal5 = new Kruskal(g5);
+        result = kruskal5.execMST();
+        System.out.println("===== RESULTADO ======");
+        kruskal5.mostrarConjuntos(result.getnNodos());
         result.mostrarGrafo();
     }
     
